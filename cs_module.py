aa718160161d4b1248b3e14c168971c7a369b48a
AlfredM91 <107662349+AlfredM91@users.noreply.github.com>
2022-07-22 16:58:12 +0800

    Submission

---
+++ cs_module.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 11:00:00 2022
+
+@author: aaron
+"""
+
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras import Sequential, Input
+from tensorflow.keras.utils import plot_model
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder,MinMaxScaler,StandardScaler
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import KNNImputer, IterativeImputer
+from sklearn.linear_model import LogisticRegression
+
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+class EDA:
+    def allcolumns(self):
+        pd.set_option('display.max_columns',None)
+    
+    def countplot(self,cat,df):
+        for i in cat:
+            plt.figure()
+            sns.countplot(df[i])
+            plt.show()
+
+    def distplot(self,con,df):
+        for i in con:
+            plt.figure()
+            sns.distplot(df[i])
+            plt.show()
+
+    def label_encoder(self,cat,df):
+        for i in cat:
+            le = LabelEncoder()
+            temp = df[i]
+            temp[temp.notnull()] = le.fit_transform(temp[temp.notnull()])
+            df[i] = pd.to_numeric(temp,errors='coerce')
+            ENCODER_PATH = os.path.join(os.getcwd(),'models',i + '_encoder.pkl')
+            pickle.dump(le,open(ENCODER_PATH,'wb'))
+        return df
+    
+    def one_hot_encoder(self,y):
+        ohe = OneHotEncoder(sparse=False)
+        if y.ndim == 1:
+            y = ohe.fit_transform(np.expand_dims(y,axis=-1))
+        else:
+            y = ohe.fit_transform(y)
+        OHE_PATH = os.path.join(os.getcwd(),'models','ohe.pkl')
+
+        with open(OHE_PATH,'wb') as file:
+            pickle.dump(OHE_PATH,file)
+        return y
+    
+    def simple_imputer(self,df,cat,con):
+        
+        df_simple = df
+        
+        for i in con:
+            df_simple[i] = df[i].fillna(df[i].median())
+
+        for i in cat:
+            df_simple[i] = df[i].fillna(df[i].mode()[0])
+
+        print(df_simple.describe().T)
+        return df_simple
+
+    def knn_imputer(self,df,cat):
+        knn = KNNImputer() 
+        df_knn = df
+        df_knn = knn.fit_transform(df)
+        df_knn = pd.DataFrame(df_knn)
+        df_knn.columns = df.columns
+        for i in cat:
+            df_knn[i] = np.floor(df_knn[i]).astype(int)
+        print(df_knn.describe().T)
+        return df_knn
+    
+    def iterative_imputer(self,df,cat):
+        ii = IterativeImputer() 
+        df_ii = df
+        df_ii = ii.fit_transform(df)
+        df_ii = pd.DataFrame(df_ii)
+        df_ii.columns = df.columns
+        for i in cat:
+            df_ii[i] = np.floor(df_ii[i]).astype(int)
+        print(df_ii.describe().T)
+        return df_ii
+    
+    def con_vs_con_features_selection(self,df,con,target,selected_features,
+                                      corr_target=0.6,figsize=(20,12)):
+        
+        cor = df.loc[:,con].corr()
+        
+        plt.figure(figsize=figsize)
+        sns.heatmap(cor,cmap=plt.cm.Reds,annot=True)
+        plt.show()
+                
+        for i in con:
+            print(i)
+            print(cor[i].loc[target])
+            if (cor[i].loc[target]) >= corr_target:
+                selected_features.append(i)
+                print(i,' ',cor[i].loc[target])
+        return selected_features
+    
+    def cat_vs_con_features_selection(self,df,con,target,selected_features,
+                                      target_score=0.6,
+                                      solver='lbfgs',max_iter=100):
+        
+        for i in con:
+            lr = LogisticRegression(solver=solver,max_iter=max_iter)   
+            lr.fit(np.expand_dims(df[target],axis=-1),df[i])
+            lr_score = lr.score(np.expand_dims(df[target],axis=-1),df[i])
+            print(i)
+            print(lr_score)
+            if lr_score >= target_score:
+                selected_features.append(i)
+        return selected_features
+    
+    def cat_vs_cat_features_selection(self,df,cat,target,selected_features,
+                                      target_score=0.6):
+        """ calculate Cramers V statistic for categorial-categorial association.
+            uses correction from Bergsma and Wicher, 
+            Journal of the Korean Statistical Society 42 (2013): 323-328
+        """
+        for i in cat:
+            cramers_confusion_matrix = pd.crosstab(df[i],df[target]).to_numpy()
+            chi2 = ss.chi2_contingency(cramers_confusion_matrix)[0]
+            n = cramers_confusion_matrix.sum()
+            phi2 = chi2/n
+            r,k = cramers_confusion_matrix.shape
+            phi2corr = max(0, phi2 - ((k-1)*(r-1))/(n-1))    
+            rcorr = r - ((r-1)**2)/(n-1)
+            kcorr = k - ((k-1)**2)/(n-1)
+            cramers_score = np.sqrt(phi2corr / min( (kcorr-1), (rcorr-1)))
+            print(i)
+            print(cramers_score)
+            if cramers_score >= target_score:
+                selected_features.append(i)
+                # print(i,' ',cramers_score)
+        return selected_features
+
+    def min_max_scaler(self,X):
+        mms = MinMaxScaler()    
+        
+        if X.ndim == 1:
+            X = mms.fit_transform(np.expand_dims(X,axis=-1))
+        else:    
+            X = mms.fit_transform(X)
+
+        MMS_PATH = os.path.join(os.getcwd(),'models','mms.pkl')
+
+        with open(MMS_PATH,'wb') as file:
+            pickle.dump(mms,file)
+        
+        return X
+    
+    def standard_scaler(self,X):
+        ss = StandardScaler()    
+        
+        if X.ndim == 1:
+            X = ss.fit_transform(np.expand_dims(X,axis=-1))
+        else:    
+            X = ss.fit_transform(X)
+
+        SS_PATH = os.path.join(os.getcwd(),'models','ss.pkl')
+
+        with open(SS_PATH,'wb') as file:
+            pickle.dump(ss,file)
+        
+        return X
+
+class ModelDevelopment:
+    def simple_dl_model(self,X_train,y_train,activation_dense='relu',
+                        activation_output='softmax',
+                        nb_node=128,dropout_rate=0.3):
+        """
+        
+        This is a simple 3 layers Deep Learning model using Sequential approach
+
+        Parameters
+        ----------
+        X_train : TYPE
+            DESCRIPTION.
+        no_class : TYPE
+            DESCRIPTION.
+        nb_node : TYPE, optional
+            DESCRIPTION. The default is 128.
+        dropout_rate : TYPE, optional
+            DESCRIPTION. The default is 0.3.
+
+        Returns
+        -------
+        model : TYPE
+            DESCRIPTION.
+
+        """
+        
+        model = Sequential()
+        model.add(Input(shape=np.shape(X_train)[1:]))
+        model.add(Dense(nb_node,activation=activation_dense))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(nb_node,activation=activation_dense))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(nb_node,activation=activation_dense))
+        model.add(BatchNormalization())
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(len(np.unique(y_train,axis=0)),
+                            activation=activation_output))
+        model.summary()
+        
+        plot = plot_model(model,show_shapes=True)
+        PLOT_PATH = os.path.join(os.getcwd(),'statics','model.png')
+        
+        return model
+
+    def save_model(self,model):
+        MODEL_PATH = os.path.join(os.getcwd(),'models','model.h5')
+        model.save(MODEL_PATH)
+        
+# class ModelEvaluation:
+#     def plot_hist(self,hist):
+        
+#         keys = list(hist.history.keys())
+        
+#         plt.figure()
+#         plt.plot(hist.history['loss'])
+#         plt.plot(hist.history['val_loss'])
+#         plt.legend(['Training Loss','Validation Loss'])
+#         plt.show()
+
+#         plt.figure()
+#         plt.plot(hist.history['acc'])
+#         plt.plot(hist.history['val_acc'])
+#         plt.legend(['Training Accuracy','Validation Accuracy'])
+#         plt.show()
---
+++ customer_segmentation.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 22 09:36:16 2022
+
+@author: aaron
+"""
+
+from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer,KNNImputer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+from tensorflow.keras import Sequential, Input
+from tensorflow.keras.utils import plot_model
+
+from cs_module import EDA, ModelDevelopment
+import matplotlib.pyplot as plt
+from datetime import datetime
+import scipy.stats as ss
+import missingno as msno
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+#%% Constants
+
+CSV_PATH = os.path.join(os.getcwd(),'Train.csv')
+LOGS_PATH = os.path.join(os.getcwd(),'logs',datetime.now().
+                         strftime('%Y%m%d-%H%M%S'))
+PLOT_PATH = os.path.join(os.getcwd(),'statics','model.png')
+
+#%% Step 1) Data Loading
+
+df = pd.read_csv(CSV_PATH)
+
+#%% Step 2) Data Inspection
+
+eda = EDA()
+eda.allcolumns()
+
+df.info()
+df.describe().T
+df.describe(include='all').T
+df.isnull().sum()
+df.duplicated().sum()
+
+cat = list(df.columns[df.dtypes=='object'])
+cat.remove('id')
+cat.extend(['day_of_month','num_contacts_in_campaign',
+            'num_contacts_prev_campaign','term_deposit_subscribed'])
+con = list(df.drop(labels=cat,axis=1))
+con.remove('id')
+
+df[cat].describe(include='all').T
+
+# eda.countplot(cat, df)
+# eda.distplot(con, df)
+
+# df[cat].boxplot()
+# df[con].boxplot()
+
+# msno.matrix(df)
+# msno.bar(df)
+
+#%% Step 3) Data Cleaning
+
+# Filling nulls
+
+df = df.replace('unknown',None)
+df.isnull().sum()
+df = df.drop(labels=['id','days_since_prev_campaign_contact'],axis=1) # too many nulls
+
+con.remove('days_since_prev_campaign_contact')
+
+# 1. Simple imputer
+
+df_simple = eda.simple_imputer(df, cat, con)
+df_simple.describe().T
+
+# 2. KNN imputer
+
+df = eda.label_encoder(cat, df)
+df_knn = eda.knn_imputer(df,cat)
+df_knn[con].describe().T
+
+# 3. Iterative imputer
+
+df_ii = eda.iterative_imputer(df,cat)
+df_ii[con].describe().T
+
+
+# We will be using Iterative Imputer
+
+# Removing outliers - nothing to remove as all values are within acceptable 
+# range despite some outliers present in balance and last_contact_duration features
+
+# Removing duplicates
+
+df_ii.duplicated().sum()
+
+# No duplicates to remove
+
+#%% Step 4) Features Selection
+
+# selected_features = eda.cat_vs_cat_features_selection(df, cat,
+#                                                       'prev_campaign_outcome',
+#                                                       [],target_score=0.01)
+
+
+# selected_features = eda.cat_vs_con_features_selection(df, con,
+#                                                       'prev_campaign_outcome',
+#                                                       selected_features,
+#                                                       target_score=0.04,
+#                                                       solver='saga')
+
+# print(selected_features)
+
+selected_features = ['job_type',
+ 'marital',
+ 'education',
+ 'default',
+ 'housing_loan',
+ 'personal_loan',
+ 'communication_type',
+ 'month',
+ 'prev_campaign_outcome',
+ 'day_of_month',
+ 'num_contacts_in_campaign',
+ 'num_contacts_prev_campaign',
+ 'term_deposit_subscribed',
+ 'customer_age']
+
+# Selected features are as listed in the variable selected_features
+
+#%% Step 5) Data Preprocessing
+
+X = df_ii[selected_features]
+y = df_ii['prev_campaign_outcome']
+
+X = eda.min_max_scaler(X)
+y = eda.min_max_scaler(np.expand_dims(y,axis=-1))
+y = eda.one_hot_encoder(y)
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,
+                                                 random_state=123)
+
+#%% Model Development
+
+md = ModelDevelopment() 
+model = md.simple_dl_model(X_train, y_train,nb_node=512)
+plot_model(model,show_shapes=True, to_file=PLOT_PATH)
+
+#%% Model Compilation
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics='acc')
+
+#%% Model Training
+
+
+tensorboard_callback = TensorBoard(log_dir=LOGS_PATH,histogram_freq=1)
+
+early_callback = EarlyStopping(monitor='val_loss',patience=10)
+
+hist = model.fit(X_train,y_train,epochs=100,validation_data=(X_test,y_test),
+                 callbacks=[tensorboard_callback,early_callback])
+
+#%%% Model Evaluation
+
+print(hist.history.keys())
+
+plt.figure()
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.xlabel('epoch')
+plt.legend(['Training Loss','Validation Loss'])
+plt.show()
+
+plt.figure()
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.xlabel('epoch')
+plt.legend(['Training Accuracy','Validation Accuracy'])
+plt.show()
+
+print(model.evaluate(X_test,y_test))
+
+# go to Anaconda prompt and type tensorboard --logdir "actual LOGS_PATH"
+# then open the localhost url
+
+#%% Model Analysis
+
+y_pred = model.predict(X_test)
+y_pred = np.argmax(y_pred,axis=1)
+y_true = np.argmax(y_test,axis=1)
+
+cm = confusion_matrix(y_true,y_pred)
+cr = classification_report(y_true,y_pred)
+
+labels = ['success','failure','other']
+disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=labels)
+disp.plot(cmap=plt.cm.Blues)
+plt.show()
+
+print(cr)
+
+# We don't have enough data, only 201 data, and too many outliers in target
+# We need at least 1k data for DL, but 10k is better
+
+#%% Model Saving
+
+md.save_model(model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
